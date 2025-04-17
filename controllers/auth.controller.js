f17a3b68460f815960ad28f5e83c63c341ce901b
Schandroid243 <schadrackngunza@gmail.com>
2025-04-17 19:15:02 +0100

    feat(package.json): Added the jsonwebtoken package to generate tokens
    feat(models/token): Added a token model to handle the access and the refresh token
    
    feat(autController): Added and successfully tested  the login method

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,8 @@
 const { validationResult } = require("express-validator");
 const { User } = require("../models/user");
+const { Token } = require("../models/token");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 exports.register = async (req, res) => {
   const errors = validationResult(req);
@@ -12,12 +15,66 @@ exports.register = async (req, res) => {
   }
 
   try {
-    let user = new User(req.body);
+    let user = new User({
+      ...req.body,
+      passwordHash: bcrypt.hashSync(req.body.password, 8),
+    });
+    user = await user.save();
+    if (!user) {
+      return res.status(500).json({
+        type: "Internal Server Error",
+        message: "Could not create a new user",
+      });
+    }
+    return res.status(201).json(user);
   } catch (error) {
+    if (error.message.includes("email_1 dup key")) {
+      return res.status(409).json({
+        type: "AuthError",
+        message: "User with that email already exists.",
+      });
+    }
     return res.status(500).json({ ype: error.name, message: error.message });
   }
 };
-exports.login = async (req, res) => {};
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      return res.status(404).json({ message: "User not found." });
+    }
+
+    if (!bcrypt.compareSync(password, user.passwordHash)) {
+      return res.status(400).json({ message: "Incorrect email or password." });
+    }
+
+    const accessToken = jwt.sign(
+      { id: user.id, isAdmin: user.isAdmin },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "24h" }
+    );
+
+    const refreshToken = jwt.sign(
+      { id: user.id, isAdmin: user.isAdmin },
+      process.env.REFRESH_TOKEN_SECRET,
+      { expiresIn: "60d" }
+    );
+
+    const token = await Token.findOne({ userId: user.id });
+    if (token) await Token.deleteOne();
+    await new Token({
+      userId: user.id,
+      accessToken,
+      refreshToken,
+    }).save();
+
+    user.passwordHash = undefined;
+    return res.json({ ...user, accessToken });
+  } catch (error) {
+    return res.stauts(500).json({ type: error.name, message: error.message });
+  }
+};
 exports.forgotPassword = async (req, res) => {};
 exports.verifyPasswordResetOtp = async (req, res) => {};
 exports.resetPassword = async (req, res) => {};
--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
-const { Schema } = require("mongoose");
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
 
 const userSchema = Schema({
   name: {
